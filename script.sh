32c51e1a16287bd57d715dd7ab60804f00077f33
Dmitry Grigoryev <dmitry.grigoryev@pushgaming.com>
2023-01-18 16:36:05 +0000

    init

---
+++ script.sh
@@ -0,0 +1,5 @@
+# Install the repository RPM:
+sudo yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+
+# Install PostgreSQL:
+sudo yum install -y postgresql14-server
